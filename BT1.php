7d05c6088e9ef0a5e93123d5bd13232466a947c3
Thuan <minhthuansh@gmail.com>
2018-03-14 16:42:46 +0700

    nop bt
    nop bt

---
+++ BT1.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$Bai1 = "Chao mung den voi lop Lap trinh Web 1";
+echo "Chao mung den voi lop Lap trinh Web 1."."<br>";
+echo "a. Lap trinh Web 1"."<br>";
+echo "b. ";
+echo substr("Chao mung den voi lop Lap trinh Web 1",0,-33)."<br>";
+echo substr("Chao mung den voi lop Lap trinh Web 1",5,-28)."<br>";
+echo substr("Chao mung den voi lop Lap trinh Web 1",10,-24)."<br>";
+echo substr("Chao mung den voi lop Lap trinh Web 1",14,-20)."<br>";
+echo substr("Chao mung den voi lop Lap trinh Web 1",18,-16)."<br>";
+?>
+
